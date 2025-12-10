488394d3618a988998e21542a0c9ff168d4cdbd8
Zoë R <126206627+ThatOneArtKid@users.noreply.github.com>
2025-12-10 04:06:34 +0000

    wip: working on input system and figuring out how to add the sql database

--- userManagement.py
+++ userManagement.py
@@ -1,12 +1,35 @@
 import sqlite3 as sql
 import bcrypt
 
+connection = sql.connect("databaseFiles/database.db")
+cursor = connection.cursor()
+
+
+# cursor.execute(
+#    "CREATE TABLE user_data(id INTEGER PRIMARY KEY autoincrement,username TEXT NOT NULL UNIQUE, password TEXT NOT NULL)"
+# )
+cursor.execute(
+    "INSERT INTO user_data(username, password) VALUES ('JohnSoftwareEngineering', 'SecurePassword_1225')"
+)
+
+user = input("user: ")
+password = input("password: ")
+
+cursor.execute(
+    "SELECT * FROM user_data WHERE username = ? AND password = ?",
+    (user, password),
+)
+SensitiveInformation = cursor.fetchall()
+for i in SensitiveInformation:
+    print(i)
+
+connection.close()
+
 
 ### example
-def getUsers():
-    con = sql.connect("databaseFiles/database.db")
-    cur = con.cursor()
-    cur.execute("SELECT * FROM id7-tusers")
-    con.close()
-    return cur
-
+# def getUsers():
+#    con = sql.connect("databaseFiles/database.db")
+#    cur = con.cursor()
+#    cur.execute("SELECT * FROM id7-tusers")
+#    con.close()
+#    return cur
